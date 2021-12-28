3e73c0158ab9d8df6cc6c3ee656f5b250d62206e
Christian Ulrich <git@chris.ullihome.de>
2021-12-28 14:48:51 +0100

    a-frame loads

--- index.mjs
+++ index.mjs
@@ -1,13 +1,20 @@
-const supportsVR = 'getVRDisplays' in navigator;
-if (supportsVR) {
-    navigator.getVRDisplays().then(function(displays) {
-        import('./xr-based/screen.mjs').then(module => {
-            module.Init();
-        });
+if (isVrSupported()) {
+    import('./xr-based/screen.mjs').then(module => {
+        module.Init();
     });
 }
 else {
     import('./screen-based/screen.mjs').then(module => {
         module.Init();
     });
+}
+function isVrSupported() {
+    return true;
+    const supportsVR = 'getVRDisplays' in navigator;
+    if (supportsVR) {
+        navigator.getVRDisplays().then(function(displays) {
+                return true;
+            });
+        }
+    return false;
 }
---
+++ xr-based/screen.mjs
@@ -0,0 +1,5 @@
+import 'https://aframe.io/releases/1.2.0/aframe.min.js';
+export function Init() {
+    var scene = document.createElement('a-frame');
+    document.body.appendChild(scene);
+}
