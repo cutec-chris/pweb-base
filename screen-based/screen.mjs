161902aef3a01d7fdec671903cc58ef21d5cf367
Christian Ulrich <git@chris.ullihome.de>
2022-01-17 21:54:17 +0100

    fix css

--- screen-based/screen.mjs
+++ screen-based/screen.mjs
@@ -2,13 +2,15 @@ import { FASTElement, customElement, html, css } from 'https://unpkg.com/@micros
 import { } from 'https://unpkg.com/@fluentui/web-components';
 const NavBar_template = html`
 <div id="NavBar_container">
-    <fluent-tree-view></fluent-tree-view>
+    <fluent-tree-view>
+        <fluent-tree-item>test</fluent-tree-item>
+    </fluent-tree-view>
 </div>
 `;
 const NavBar_styles = css`
-NavBar_container {
-    height: "100%",
-    width: 300px
+#NavBar_container {
+    height: "100%";
+    width: 300px;
 }
 `;
 export class NavBar extends FASTElement {
