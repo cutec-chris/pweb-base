ece5ffb4b365b754bd6b52cfe69c07299b96f5ce
Chris <ulrich@tcs-germany.de>
2022-01-31 14:36:07 +0100

    show splash correct

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ function doInit() {
     Screen.Init();
     let nav = document.createElement('promet-navbar');
     document.body.appendChild(nav);
+    window.hideSplash();
 }
 function isVrSupported() {
     //return true;
@@ -21,4 +22,12 @@ if (isVrSupported()) {
     import('./screen-based/screen.mjs').then(module => {
         Screen = module;
     }).then(doInit);
+}
+//show Splash screen
+var splash = document.createElement('div');
+splash.setAttribute("style","background-color: white;width: 99%;height: 99%;position: absolute;top:0;bottom:0;left:0;right:0;margin:auto;border: 0;font-family:arial;font-size:25px;font-weight:bold;display: flex; align-items: center; justify-content: center; text-align: center");
+splash.innerHTML = document.title;
+document.body.appendChild(splash);
+window.hideSplash = function() {
+    document.body.removeChild(splash);
 }
--- screen-based/screen.mjs
+++ screen-based/screen.mjs
@@ -22,21 +22,4 @@ export class NavBar extends FASTElement {
 }
 FASTElement.define(NavBar);
 export function Init() {
-    showSplash();
-}
-function showSplash() {
-    //show Splash screen
-    var splash = document.createElement('div');
-    splash.setAttribute("style","background-color: white;width: 99%;height: 99%;position: absolute;top:0;bottom:0;left:0;right:0;margin:auto;border: 0;font-family:arial;font-size:25px;font-weight:bold;display: flex; align-items: center; justify-content: center; text-align: center");
-    splash.innerHTML = document.title;
-    document.body.appendChild(splash);
-    function hideSplash(){
-        document.body.removeChild(splash);
-    }
-    window.onload = function() {
-        hideSplash();
-    }
-    window.setTimeout(function(){
-        hideSplash();
-    },50)
 }
