d12a60c67681634226f43adf95590f8382e02614
Christian U <christian@ullihome.de>
2022-02-12 09:12:33 +0100

    add addlink

--- xr-based/screen.mjs
+++ xr-based/screen.mjs
@@ -2,6 +2,8 @@ import 'https://aframe.io/releases/1.2.0/aframe.min.js';
 AFRAME.registerComponent('NavBar', {
     init: function () {
     }
+    addLink: function(id,name,target) {
+    }
 }
 export function Init() {
     var scene = document.createElement('a-frame');
