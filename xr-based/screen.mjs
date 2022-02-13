a59f77377c60263e629b8d70c496189a1f845558
Christian U <christian@ullihome.de>
2022-02-13 18:53:28 +0100

    basic scene works

--- xr-based/screen.mjs
+++ xr-based/screen.mjs
@@ -1,12 +1,20 @@
 import 'https://aframe.io/releases/1.2.0/aframe.min.js';
-AFRAME.registerComponent('NavBar', {
+AFRAME.registerComponent('promet-navbar', {
     init: function () {
     },
     addLink: function(id,name,target) {
     },
 });
-let Scene = null;
+let Scene,Camera,Viewer = null;
 export function Init() {
-    Scene = document.createElement('a-frame');
+    Scene = document.createElement('a-scene');
     document.body.appendChild(Scene);
+    Viewer = document.createElement('a-entity');
+    Scene.appendChild(Viewer);
+    Camera = document.createElement('a-entity');
+    Viewer.appendChild(Camera);
+    Camera.setAttribute('position','0 1.9 0')
+    Camera.setAttribute('camera','')
+    Camera.setAttribute('look-contols','')
+    Camera.setAttribute('wasd-contols','')
 }
